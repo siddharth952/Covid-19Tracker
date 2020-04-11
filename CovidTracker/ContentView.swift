aa62ab6c30171fa9fc7abb6d256ae9b3e9ccdabc
Siddharth <40488679+siddharth952@users.noreply.github.com>
2020-04-11 22:44:03 +0530

    Initial Commit

---
+++ CovidTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidTracker
+//
+//  Created by Siddharth sen on 4/11/20.
+//  Copyright © 2020 HalfPolygon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
